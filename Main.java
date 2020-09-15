4749e2e865fcf082888be8d5ae3d8b5fcd1c86c7
fiscn <fiscn2709@wrdsb.ca>
2020-09-15 18:00:34 +0000

    Question A2Q3 or "Ascending order" Program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * Takes two integers from user and finds the ascending order 
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create Scanner for user input 
+     Scanner input = new Scanner(System.in);  
+
+     // Prompt the user to write down a integer 
+     System.out.println("Please enter an integer:");
+     int Integer_1 = input.nextInt(); 
+
+     // prompt the user to enter another integer 
+     System.out.println("Please enter another integer:"); 
+     int Integer_2 = input.nextInt(); 
+
+     // check to see where integers are placed for ascending order 
+     if (Integer_1 > Integer_2) {
+       System.out.println("Your numbers in ascending order are " + Integer_1 + ", " + Integer_2);
+     } else {
+       System.out.println("Your numbers in ascending order are " + Integer_2 + ", " + Integer_1);
+     }
+
+     
   }
 }
